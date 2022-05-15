92595f1ba35cc8984cc4a8a0cd70a6d500ed9f81
Jeff Ripke <jripke74@gmail.com>
2022-05-15 09:00:10 -0700

    add logic for mobile menu

--- shared.js
+++ shared.js
@@ -2,6 +2,8 @@ const backdrop = document.querySelector('.backdrop');
 const modal = document.querySelector('.modal');
 const nodalNoButton = document.querySelector('.modal__action--negative');
 const selectPlanButtons = document.querySelectorAll('.plan button');
+const toggleButton = document.querySelector('.toggle-button');
+const mobileNav = document.querySelector('.mobile-nav');
 
 for (var i = 0; i < selectPlanButtons.length; i++) {
   selectPlanButtons[i].addEventListener('click', function() {
@@ -9,6 +11,11 @@ for (var i = 0; i < selectPlanButtons.length; i++) {
     backdrop.style.display = 'block';
   });
 }
+
+backdrop.addEventListener('click', function() {
+  mobileNav.style.display = 'none';
+  closeModal();
+});
 
 function closeModal() {
   backdrop.style.display = 'none';
@@ -18,3 +25,8 @@ function closeModal() {
 backdrop.addEventListener('click', closeModal);
 
 nodalNoButton.addEventListener('click', closeModal);
+
+toggleButton.addEventListener('click', function() {
+  mobileNav.style.display = 'block';
+  backdrop.style.display = 'block';
+});
